49143c5f50b79f0e6398ffe6e712af5a50ed7c4f
zhoucumt <553817478@qq.com>
2020-09-13 11:57:45 +0800

    change

--- src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function greeter(person) {
-    return 'hello, ' + person;
-}
-var a = null;
-// symbol
-var sym1 = Symbol('key1');
-// 使用 BigInt 的时候，必须添加 ESNext 的编译辅助库
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,24 @@ function greeter(person: string) {
     return 'hello, ' + person;
 }
 const a: any = null;
+let b: any = 'hello';
+console.log(b.anne);
+
+let c: string | number;
+c = 9;
+c = 'a';
 
 // symbol
 const sym1 = Symbol('key1');
 
 // 使用 BigInt 的时候，必须添加 ESNext 的编译辅助库
+
+interface Person {
+    name: string,
+    age: number
+};
+
+let tom: Person = {
+    name: 'Tom',
+    age: 3
+}
